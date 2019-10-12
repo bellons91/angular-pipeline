dce44dad78417b9d955062c3f29814cafe4b8fa0
Davide <bellons91@yahoo.it>
2019-10-12 12:05:07 +0200

    Update header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center">
       <h1>
-       Hello Azure Pipelines!!
+       Hello Azure Pipelines :)
       </h1>
       <img width="300"  src="http://www.mattruma.com/wp-content/uploads/2019/04/528389819366_e7a0672f0480b3e98d21_512.png">
     </div>
